41f19891d3118acd0f99e480b790fa6cd86e6fce
Joshua Pierce <jpierce@myluckyday.fr>
2019-03-15 10:40:21 +0100

    finished gendoc, regened docs

--- bin/gendoc.sh
+++ bin/gendoc.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env sh
+
+command -v jazzy >/dev/null 2>&1 || {
+  echo >&2 "[GENDOC] Jazzy not found, attempting to install..."
+  gem install jazzy
+}
+
+BASE_DIR="$(dirname $(greadlink -f $0))"
+SPEC_PATH="$BASE_DIR/../PaylineSDK.podspec"
+DOC_PATH="$BASE_DIR/../Documentation/jazzydoc"
+
+echo '[GENDOC] Removing old docs...'
+rm -rf "$DOC_PATH"
+
 echo '[GENDOC] Generating API reference documentation using Jazzy...'
-DIR="$(dirname $(greadlink -f $0))"
-jazzy --podspec "$DIR/../PaylineSDK.podspec" -o "$DIR/../Documentation/jazzydoc"
+jazzy --podspec "$SPEC_PATH" -o "$DOC_PATH"
