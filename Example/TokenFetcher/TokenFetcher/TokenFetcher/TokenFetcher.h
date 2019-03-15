86d3cffd6f1ccb6485191a32844ab3fb7cb9010f
Joshua Pierce <jpierce@myluckyday.fr>
2019-03-15 11:02:26 +0100

    added TokenFetcher module

---
+++ Example/TokenFetcher/TokenFetcher/TokenFetcher/TokenFetcher.h
@@ -0,0 +1,19 @@
+//
+//  TokenFetcher.h
+//  TokenFetcher
+//
+//  Created by MacBook on 3/15/19.
+//  Copyright © 2019 Payline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TokenFetcher.
+FOUNDATION_EXPORT double TokenFetcherVersionNumber;
+
+//! Project version string for TokenFetcher.
+FOUNDATION_EXPORT const unsigned char TokenFetcherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TokenFetcher/PublicHeader.h>
+
+
